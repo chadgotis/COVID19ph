31096d9fe39ff03c6213cfc2350afc2fded9dbcf
Chad Gotis <Chadgotis97@gmail.com>
2020-04-14 02:26:25 +0800

    Updates

---
+++ precache-manifest.d88d7367ebd7ca9ea1a8338fbfff3fbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4ed1aa26b4c8d02e926a2232f6c102",
+    "url": "/Covid19ph/index.html"
+  },
+  {
+    "revision": "f9dba51a200f67f9699d",
+    "url": "/Covid19ph/static/css/main.9e7a39cc.chunk.css"
+  },
+  {
+    "revision": "f87b509bf4a0cca17de5",
+    "url": "/Covid19ph/static/js/2.9e3173c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid19ph/static/js/2.9e3173c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9dba51a200f67f9699d",
+    "url": "/Covid19ph/static/js/main.e6b27329.chunk.js"
+  },
+  {
+    "revision": "4f0f31cac787b5e6a490",
+    "url": "/Covid19ph/static/js/runtime-main.8cdf3163.js"
+  }
+]);
